aa7c25f34a62b5b98637cea2f3eaea5ea04f2acd
Sortoite <zachatatem@gmail.com>
2022-03-24 01:21:04 -0500

    tha good stuff

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,11 +1,13 @@
 export default function Menu() {
   return (
     <div>
-      <div className="fixed bottom-0 w-full p-12 flex justify-start font-bold text-xl">
-        <a href="#contact">Startup Dev</a>
-      </div>
-      <div className="fixed bottom-0 w-full p-12 flex justify-end font-bold text-xl">
-        <a href="#contact">Contact</a>
+      <div className="fixed bottom-0 w-full p-12 flex  font-bold text-xl">
+        <div className="flex w-full">
+          <a href="#top" className="flex-grow">
+            Startup Dev
+          </a>
+          <a href="#contact">Contact</a>
+        </div>
       </div>
     </div>
   );
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -11,6 +11,7 @@ export default function Index() {
           style={{
             backgroundImage: "url(/images/bg-slide-building.jpg)",
           }}
+          id="top"
         >
           <div className="m-auto max-w-5xl p-12">
             <div className="m-auto max-w-5xl ">
@@ -40,14 +41,49 @@ export default function Index() {
       </Slide>
       <Slide>
         <div
-          className="bg-cover h-screen"
+          className="bg-cover h-cover"
           style={{
-            backgroundImage: "url(/images/mk-s-wKS46KBpolg-unsplash.jpeg)",
+            backgroundImage:
+              "url(/images/annie-spratt-6a3nqQ1YwBw-unsplash.jpeg)",
           }}
         >
           <div className="m-auto max-w-5xl p-12">
-            <div className="text-5xl uppercase font-bold text-white space-x-3">
+            <div className="text-3xl uppercase text-gray-400 space-x-3">
               Trusted by
+            </div>
+            <div className="grid grid-cols-3 lg:grid-cols-4 gap-4 lg:gap-24 py-24">
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-google1.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-youtube.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-facebook.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-ea.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-okta.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-envoy.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-hackerone.svg"
+              ></img>
+              <img
+                className="w-80 p-0"
+                src="/images/trusted/white/logo-havo.svg"
+              ></img>
             </div>
           </div>
         </div>
@@ -67,7 +103,7 @@ export default function Index() {
               </div>
               <div className="mb-4">
                 <input
-                  className="bg-gray-100 border border-gray-200 rounded py-4 px-4 mb-3 w-full"
+                  className="bg-gray-100 border border-gray-200 rounded py-4 px-4 mb-3 w-full text-gray-800"
                   placeholder="Your email address"
                 />
               </div>
@@ -81,7 +117,13 @@ export default function Index() {
               <div className="flex-grow"></div>
               <div className="flex">
                 <div className="mr-2">
-                  <img className="w-6" src="/images/github.svg" alt="Github" />
+                  <a href="https://github.com/startupdotdev">
+                    <img
+                      className="w-6"
+                      src="/images/github.svg"
+                      alt="Github"
+                    />
+                  </a>
                 </div>
                 <div className="mr-2">
                   <a href="https://twitter.com/startupdotdev">
