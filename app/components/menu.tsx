81b5e7f02f8aec19a2dcfa58fe81f9c70b5b3dac
Sortoite <zachatatem@gmail.com>
2022-11-29 10:00:49 -0500

    Move floating footer down a little to not overlap with initial header content on mobile

--- app/components/menu.tsx
+++ app/components/menu.tsx
@@ -1,14 +1,12 @@
 export default function Menu() {
   return (
     <div>
-      <div className="fixed bottom-0 w-full p-12 flex  font-bold text-xl">
+      <div className="fixed bottom-0 w-full px-12 pt-12 pb-8 flex  font-bold text-xl">
         <div className="flex w-full text-white">
-          <a href="#top" className="flex-grow text">
+          <a href="#top" className="flex-grow">
             startup.security
           </a>
-          <a className="texxt" href="#contact">
-            Contact
-          </a>
+          <a href="#contact">Contact</a>
         </div>
       </div>
     </div>
